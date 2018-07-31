f2bed54cbd5aaa2607587c22c37395c83a5b7386
VladMihai28 <mihai.vlad28@gmail.com>
2018-07-31 08:43:40 +0300

    Add first iteration of source code

---
+++ app/src/main/java/vlad/mihai/com/speedruns/MainActivity.java
@@ -0,0 +1,20 @@
+package vlad.mihai.com.speedruns;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout)).setTitle(getString(R.string.app_name));
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+    }
+}
